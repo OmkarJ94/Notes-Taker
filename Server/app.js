7a6cb45e4056b420b822dc9ae5b97653261bc726
Omkar Jadhav <oj86029@gmail.com>
2022-01-24 20:00:19 +0530

    updated

--- Server/app.js
+++ Server/app.js
@@ -4,15 +4,8 @@ require("dotenv").config({})
 var cookieParser = require('cookie-parser')
 app.use(cookieParser())
 require("./db/conn")
-const PORT = process.env.PORT || 5000
+const PORT =  5000
 app.use(express.json())
 app.use(require("./Router/auth"))
-// if (process.env.NODE_ENV == "production") {
 
-//     app.use(express.static("Client/build"));
-//     const path = require("path");
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
-//     })
-// }
 app.listen(5000);
